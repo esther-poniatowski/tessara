d51017a3635b5ff74573141bf50633bff9dbaa0f
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2025-02-11 06:40:04 +0100

    chore(*): Initialize directory structure

---
+++ src/tessara/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+tessara
+=======
+
+Modules
+-------
+core
+    Define the foundational components of the Tessara framework (API).
+
+See Also
+--------
+test_tessara
+    Tests for the entire package.
+"""
---
+++ tests/test_tessara/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+test_tessara
+============
+
+Tests for the tessara package.
+
+Modules
+-------
+test_core
+test_validation
+test_handling
+
+See Also
+--------
+tessara
+
+Notes
+-----
+This test package is imported by Sphinx to extract docstrings for documenting tests. This
+documentation provides additional examples and explanations for the tested modules and functions.
+"""
