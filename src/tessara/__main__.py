a60ea71586cb988470d796449c931df88714b068
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2025-06-08 12:54:10 +0200

    feat(__main__): Add entry point

---
+++ src/tessara/__main__.py
@@ -0,0 +1,17 @@
+"""
+Command-line entry point for the `tessara` package.
+
+Usage
+-----
+To invoke the package::
+
+    python -m tessara
+
+
+See Also
+--------
+tessara.cli: Command-line interface module for the package.
+"""
+from .cli import app
+
+app()
